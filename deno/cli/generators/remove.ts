48e1c104e08b6d7b20dd69f6473f2a4e79d6e909
Dmitri Grabov <d.grabov@gmail.com>
2025-08-09 15:45:31 +0100

    Adding project to cli

--- deno/cli/generators/remove.ts
+++ deno/cli/generators/remove.ts
@@ -1,37 +1,35 @@
-import { Command, type StringType } from '@cliffy/command'
+import { Command } from '@cliffy/command'
 import { Input } from '@cliffy/prompt'
-import type { Project } from '../lib/project.ts'
-
-type CommandType = Command<
-  void,
-  void,
-  void,
-  [StringType & string],
-  void,
-  {
-    number: number
-    integer: number
-    string: string
-    boolean: boolean
-    file: string
-  },
-  void,
-  undefined
->
+import type { SkmtcRoot } from '../lib/skmtc-root.ts'
+import invariant from 'tiny-invariant'
 
 export const description = 'Remove a generator from the stack'
 
-export const toRemoveCommand = (project: Project): CommandType => {
+export const toRemoveCommand = (skmtcRoot: SkmtcRoot) => {
   const command = new Command()
     .description(description)
     .example('Remove RTK Query generator from the stack', 'remove @skmtc/rtk-query')
-    .arguments('<generator:string>')
-    .action((_options, generator) => project.removeGenerator({ packageName: generator }))
+    .arguments('<project:string> <generator:string>')
+    .action((_options, project, generator) => {
+      return skmtcRoot.projects
+        .find(({ name }) => name === project)
+        ?.removeGenerator({ packageName: generator })
+    })
 
   return command
 }
 
-export const toRemovePrompt = async (project: Project) => {
+export const toRemovePrompt = async (skmtcRoot: SkmtcRoot) => {
+  const projectName = await Input.prompt({
+    message: 'Select project to remove generator from',
+    list: true,
+    suggestions: skmtcRoot.projects.map(({ name }) => name)
+  })
+
+  const project = skmtcRoot.projects.find(project => project.name === projectName)
+
+  invariant(project, 'Project not found')
+
   const generator: string = await Input.prompt({
     message: 'Remove a generator from the stack',
     list: true,
