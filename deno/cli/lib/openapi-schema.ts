aa165f155ece1b38c948629cec8e1934ea6df2d8
Dmitri Grabov <d.grabov@gmail.com>
2025-11-06 22:15:26 +0000

    Remove unused code

--- deno/cli/lib/openapi-schema.ts
+++ deno/cli/lib/openapi-schema.ts
@@ -1,18 +1,8 @@
 import { exists } from '@std/fs/exists'
-import type { SkmtcRoot } from '@/lib/skmtc-root.ts'
-import { getApiWorkspacesWorkspaceName } from '@/services/getApiWorkspacesWorkspaceName.generated.ts'
-import { patchApiSchemasSchemaId } from '@/services/patchApiSchemasSchemaId.generated.ts'
-import { CreateSchemaBodyFile } from '@/types/createSchemaBodyFile.generated.ts'
-import { createApiSchemas } from '@/services/createApiSchemas.generated.ts'
 
 type OpenApiSchemaArgs = {
   path: string
   contents: string
-}
-
-type UploadArgs = {
-  projectName: string
-  skmtcRoot: SkmtcRoot
 }
 
 export class OpenApiSchema {
@@ -38,50 +28,6 @@ export class OpenApiSchema {
     const contents = await Deno.readTextFile(path)
 
     return new OpenApiSchema({ path, contents })
-  }
-
-  async upload({ projectName, skmtcRoot }: UploadArgs) {
-    const workspace = await getApiWorkspacesWorkspaceName({
-      workspaceName: projectName,
-      supabase: skmtcRoot.manager.auth.supabase
-    })
-
-    const schemaId = workspace.schema.id
-
-    if (schemaId) {
-      const serverFilePath = await skmtcRoot.apiClient.uploadSchemaFile({
-        openApiSchema: this,
-        schemaId
-      })
-
-      const file: CreateSchemaBodyFile = {
-        type: 'file',
-        fileContent: serverFilePath
-      }
-
-      const schema = await patchApiSchemasSchemaId({
-        schemaId,
-        supabase: skmtcRoot.manager.auth.supabase,
-        body: {
-          id: schemaId,
-          file: file
-        }
-      })
-
-      return schema
-    } else {
-      const serverFilePath = await skmtcRoot.apiClient.uploadSchemaFile({
-        openApiSchema: this,
-        schemaId: Date.now().toString()
-      })
-
-      const schema = await createApiSchemas({
-        supabase: skmtcRoot.manager.auth.supabase,
-        body: { type: 'file', fileContent: serverFilePath }
-      })
-
-      return schema
-    }
   }
 
   async write() {
